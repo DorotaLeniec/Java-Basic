03ec26d356264ca9e73fed3a35decad35a339277
DorotaLeniec <dorleniec@gmail.com>
2017-07-08 09:27:04 +0200

    Tablice Zadanie 1 - podstawy

---
+++ src/Loops/Zadanie1.java
@@ -0,0 +1,35 @@
+package Loops;
+
+/**
+ * Created by RENT on 2017-07-08.
+ */
+public class Zadanie1 {
+    public static void main(String[] args) {
+        System.out.println("ZADANIE 1 a");
+        for(int i=1;i<=10;i++){
+            System.out.print(i);
+        }
+
+        System.out.println("ZADANIE 1 b");
+        for(int i=1;i<=20;i++){
+            if(i%2==0){
+                System.out.println("Parzyste " + i);
+            } else{
+                System.out.println("Nieparzyste "  + i);
+            }
+        }
+
+
+        System.out.println("ZADANIE 1 c");
+        for(int i=3; i<=100;i++){
+            if(i%3==0 || i%5==0){
+                System.out.print(i +", ");
+            }
+        }
+
+        System.out.println("ZADANIE 1 d");
+        for(char i = 'a'; i<'z';i++){
+            System.out.print((char)i + " ");
+        }
+    }
+}
