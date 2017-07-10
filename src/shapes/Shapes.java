3a78feeb6678eb7060d4891659db25c0b94ec0c8
Dorota Leniec <dorleniec@gmail.com>
2017-07-10 20:50:07 +0200

    drukowanie prostyh shapów

--- src/shapes/Shapes.java
+++ src/shapes/Shapes.java
@@ -7,7 +7,7 @@ public class Shapes {
 
     public static void main(String[] args) {
 //        printRec(2,4);
-        printTri(3);
+        printTri(5);
     }
     public static void printRec(int a, int b){
         for(int i = 0;i<a;i++){
@@ -15,16 +15,25 @@ public class Shapes {
         }
     }
 
+
     private static void printLien(int b) {
         for(int j = 0;j<b;j++){
             System.out.print("*");
         }
-        System.out.println();
     }
 
     public static void printTri(int height){
         for(int i=1;i<=height;i++){
+            printSpaces((height-i)/2);
             printLien(i);
+            printSpaces((height-i)/2);
+            System.out.println();
+        }
+    }
+
+    public static void printSpaces(int length){
+        for(int j=0;j<length;j++){
+            System.out.print(" ");
         }
     }
 }
